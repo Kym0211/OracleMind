c8560cc662236adb66a1e36e4850964db5add4e1
Kym0211 <kavyam.ksingh@gmail.com>
2025-07-18 11:24:36 +0530

    authentication completed

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,8 +2,7 @@ import { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import bs58 from 'bs58';
 import {PublicKey} from '@solana/web3.js';
-import {verifySignIn} from '@solana/wallet-standard-util'
-import * as ed25519 from '@noble/ed25519'
+import nacl from 'tweetnacl';
 
 // ed25519.etc.sha512Sync = sha512;
 export const authOptions: NextAuthOptions = {
@@ -18,7 +17,6 @@ export const authOptions: NextAuthOptions = {
             async authorize(credentials) {
                 try {
                     console.log("Auth started: ", credentials);
-                    // Assume frontend passes { address, signature, message }
                     const input = credentials?.input ? JSON.parse(credentials.input) : {};
                     const output = credentials?.output ? JSON.parse(credentials.output) : {};
                     const { address, signature, message } = input;
@@ -26,15 +24,19 @@ export const authOptions: NextAuthOptions = {
                     const publicKey = new PublicKey(address).toBytes();
                     const messageBytes = new TextEncoder().encode(message);
                     const signatureBytes = bs58.decode(signature);
-                    const msgBytes = new TextEncoder().encode(message);
-                    
+                    const isVerified = nacl
+                        .sign
+                        .detached
+                        .verify(
+                            messageBytes,
+                            signatureBytes,
+                            publicKey
+                        )
 
-                    const isVerified = verifySignIn(input, output);
+                    
                     if (!isVerified) return null;
 
-                    // 2. Optionally: Parse/validate the nonce here
-
-                    // 3. Return user object
+                    
                     return { id: address, name: address };
                 } catch (e) {
                     console.error(e);
